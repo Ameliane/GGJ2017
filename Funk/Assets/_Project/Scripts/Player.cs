dff49254c899adacfdb5ad1b8f2c4850e208e048
Toboshi <evan.welchner@gmail.com>
2017-01-20 20:10:36 -0500

    Added basic character controller. (Also ignore the library.)

---
+++ Funk/Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float m_Speed = 10;
+
+    [SerializeField]
+    float m_TurnSpeed = 80;
+
+    [SerializeField]
+    float m_JumpPower = 7;
+
+    [SerializeField]
+    float m_MaxGroundAngle = 60;
+    bool m_IsOnGround = true;
+
+    Rigidbody m_Body = null;
+
+	void Start ()
+    {
+        m_Body = GetComponent<Rigidbody>();
+	}
+
+    void Update ()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * m_TurnSpeed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && m_IsOnGround)
+        {
+            m_Body.AddForce(Vector3.up * m_JumpPower, ForceMode.VelocityChange);
+            m_IsOnGround = false;
+        }
+    }
+	
+	void FixedUpdate ()
+    {
+        m_Body.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * m_Speed, ForceMode.Acceleration);
+        m_Body.velocity = Vector3.Scale(m_Body.velocity, new Vector3(0.95f, 1, 0.95f)); //Drag, but without vertical drag!
+    }
+
+    void OnCollisionEnter(Collision aHit)
+    {
+        if (Vector3.Angle(aHit.contacts[0].normal, Vector3.up) < m_MaxGroundAngle)
+        {
+            m_IsOnGround = true;
+        }
+    }
+}
