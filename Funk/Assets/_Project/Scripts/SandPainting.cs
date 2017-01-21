cd6f97a4436c3f7e88a155531754103ccebbca58
Ameliane <rachel.joannis@gmail.com>
2017-01-20 21:47:33 -0500

    Fixed a few issues with SandPainting.

--- Funk/Assets/_Project/Scripts/SandPainting.cs
+++ Funk/Assets/_Project/Scripts/SandPainting.cs
@@ -6,20 +6,23 @@ public class SandPainting : MonoBehaviour
 
     public enum SandColor
     {
-        BEIGE,
-        AQUA,
-        PURPLE,
+        DEFAULT,
+        BOUNCE,
+        FAST,
         SIZE
     };
 
     public Terrain m_Terrain;
     public SandColor m_SandColor;
-    public int m_Radius = 40;
+    public float m_Radius = 2;
+
+    private float[,,] m_OriginalMap;
 
     // Use this for initialization
     void Start()
     {
-
+        m_OriginalMap = m_Terrain.terrainData.GetAlphamaps(
+            0, 0, m_Terrain.terrainData.alphamapWidth, m_Terrain.terrainData.alphamapHeight);
     }
 
     // Update is called once per frame
@@ -44,24 +47,43 @@ public class SandPainting : MonoBehaviour
         pos.y = relativePos.y / m_Terrain.terrainData.size.y;
         pos.z = relativePos.z / m_Terrain.terrainData.size.z;
 
+        // If outside of map, don't paint
+        if (relativePos.x < 0 || relativePos.z < 0)
+        {
+            Debug.Log("Out of Bounds");
+            return;
+        }
+        if (relativePos.x > m_Terrain.terrainData.size.x || relativePos.z > m_Terrain.terrainData.size.z)
+        {
+            Debug.Log("Out of Bounds");
+            return;
+        }
+
         // we set an offset so that all the sampled terrain is under this game object
-        int size = m_Radius * 2;
+        int size = Mathf.RoundToInt(m_Radius * 2);
         int offset = size / 2;
 
         int posXInTerrain = (int)(pos.x * m_Terrain.terrainData.alphamapWidth);
         int posYInTerrain = (int)(pos.z * m_Terrain.terrainData.alphamapHeight);
 
         // TODO: Deal with extremities
+        int x = posXInTerrain - offset;
+        x = x < 0 ? 0 : x;
+        int y = posYInTerrain - offset;
+        y = y < 0 ? 0 : y;
 
-        float[,,] splatMap = m_Terrain.terrainData.GetAlphamaps(posXInTerrain - offset, posYInTerrain - offset, size, size);
+        int width = x + size > m_Terrain.terrainData.alphamapWidth ? m_Terrain.terrainData.alphamapWidth - x - 1 : size;
+        int height = y + size > m_Terrain.terrainData.alphamapHeight ? m_Terrain.terrainData.alphamapHeight - y - 1 : size;
+
+        float[,,] splatMap = m_Terrain.terrainData.GetAlphamaps(x, y, width, height);
 
         // we set each sample of the terrain in the size
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < size; j++)
+            for (int j = 0; j < height; j++)
             {
                 // If distance between player and place on the splatMap is less than the specified Radius
-                float dist = Vector2.Distance(new Vector2(pos.x, pos.y), new Vector2(i, j));
+                float dist = Vector2.Distance(new Vector2(size / 2, size / 2), new Vector2(i, j));
 
                 if (dist < m_Radius)
                 {
@@ -77,6 +99,11 @@ public class SandPainting : MonoBehaviour
             }
         }
 
-        m_Terrain.terrainData.SetAlphamaps(posXInTerrain - offset, posYInTerrain - offset, splatMap);
+        m_Terrain.terrainData.SetAlphamaps(x, y, splatMap);
+    }
+
+    private void OnApplicationQuit()
+    {
+        m_Terrain.terrainData.SetAlphamaps(0, 0, m_OriginalMap);
     }
 }
